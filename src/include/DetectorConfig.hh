cff88928564f64ffd770d87fdd8fb593e805cfdd
Bruno Gelli <62032408+BrunoGelli@users.noreply.github.com>
2026-03-11 17:12:32 -0700

    Avoid MT config-ntuple fill on workers to stop Analysis_W001 spam

--- src/include/DetectorConfig.hh
+++ src/include/DetectorConfig.hh
@@ -1,10 +1,15 @@
-// DetectorConfig.hh
 #ifndef DETECTOR_CONFIG_HH
 #define DETECTOR_CONFIG_HH
 
 struct DetectorConfig {
-    int sizeX, sizeY, sizeZ;
-    double pixelSizeY, pixelSizeZ;
+    double worldRadiusCm;
+    double worldHeightCm;
+    double lappdSizeCm;
+    double driftDistanceCm;
+    double guidePitchCm;
+    double wallThicknessMm;
+    double wallReflectivity;
+    double lappdPixelCm;
 };
 
 #endif // DETECTOR_CONFIG_HH
